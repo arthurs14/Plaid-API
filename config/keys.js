b5c24141033782b25e35c667419b7122ec352a1c
arthurs14 <arthurcs426@gmail.com>
2021-06-24 12:33:41 -0700

    working on getting the link token

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PLAID_CLIENT_ID: '60d3d7898953d9000fa5fa80',
+  PLAID_SECRET: '8593beebeaec6166bf343e0719526e',
+};
--- server.js
+++ server.js
@@ -1,5 +1,38 @@
-require('dotenv').config();
 const express = require('express');
+const keys = require('./config/keys');
 const plaid = require('plaid');
+
 const app = express();
-const PORT = 3001;
+app.use(express.json());
+
+const client = new plaid.Client({
+  clientID: keys.PLAID_CLIENT_ID,
+  secret: keys.PLAID_SECRET,
+  env: plaid.environments.sandbox,
+});
+
+app.post('/create_link_token', async (request, response) => {
+  try {
+    // Get the client_user_id by searching for the current user
+    // const user = await User.find();
+    // const clientUserId = user.id;
+
+    // Create the link_token with all of your configurations
+    const tokenResponse = await client.createLinkToken({
+      user: {
+        client_user_id: 'test-user-id',
+      },
+      client_name: 'Plaid Test App',
+      products: ['transactions'],
+      country_codes: ['US'],
+      language: 'en',
+      webhook: 'https://webhook.sample.com',
+    });
+
+    response.json(tokenResponse)
+
+  } catch(e) {
+    // display error on client
+    return response.send({ error: e.message });
+  }
+});
